496a7d0f4d8463b6051e59fe719cc62d46b16a19
Sandhya602 <sandhyabv31@gmail.com>
2025-10-05 17:17:47 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,28 @@
+"""utils.py
+Helper functions for plotting and evaluation.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import silhouette_score
 
+
+
+
+def plot_elbow(inertias, ks, out_path=None):
+plt.figure()
+plt.plot(ks, inertias, marker="o")
+plt.xlabel("k")
+plt.ylabel("Inertia")
+plt.title("Elbow Method for K selection")
+if out_path:
+plt.savefig(out_path)
+else:
+plt.show()
+
+
+
+
+def compute_silhouette(X, labels):
+if len(set(labels)) == 1:
+return float('nan')
+return silhouette_score(X, labels)
